04bed24cbe88448383f30561edcb507930b52faf
Nahum <nahummejiad@gmail.com>
2026-04-01 17:25:11 -0600

    Modified: Change strategy on push on games list and changed properties for signals

--- src/app/features/games/games-list/games-list.ts
+++ src/app/features/games/games-list/games-list.ts
@@ -1,26 +1,31 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { VideogameService } from '../../../shared/services/videogame.service';
 import { Game, GameList } from '../../../shared/interfaces/games.interface';
 import { GameCard } from "../../../core/components/game-card/game-card";
+import { Loader } from "../../../core/components/loader/loader";
 
 @Component({
   selector: 'app-games-list',
-  imports: [GameCard],
+  imports: [GameCard, Loader],
   templateUrl: './games-list.html',
   styleUrl: './games-list.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GamesList implements OnInit {
   private readonly gamesService = inject(VideogameService);
 
-  public games: Game[] = [];
-  public totalGames: number = 0;
-  public nextPage: string | null = null;
+  public games = signal<Game[]>([]);
+  public totalGames = signal<number>(0);
+  public nextPage = signal<string | null>(null);
+  public isLoading = signal<boolean>(false);
 
   ngOnInit(): void {
-    this.gamesService.getGames().subscribe((response: GameList)=> {
-      this.games = response.results;
-      this.totalGames = response.count;
-      this.nextPage = response.next;
-    })
+    this.isLoading.set(true);
+    this.gamesService.getGames().subscribe((response: GameList) => {
+      this.games.set(response.results);
+      this.totalGames.set(response.count);
+      this.nextPage.set(response.next);
+      this.isLoading.set(false);
+    });
   }
 }
